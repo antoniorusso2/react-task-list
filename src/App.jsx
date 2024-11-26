fd0df10725f3d18b51dcfa6450d70733e2759a73
antoniorusso2 <russoantonio303@outlook.it>
2024-11-26 16:30:06 +0100

    - css reset  | - jsx script for showing list items in page | - moved data folder in src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './App.css';
+import { tasks } from './data/tasks';
+
+const completedTasks = tasks.filter((task) => task.state === 'completed');
+const incompleteTasks = tasks.filter((task) => task.state !== 'completed');
 
 function App() {
-  return;
+  return (
+    <>
+      {/* task da completare */}
+      <ul className="task-list">
+        <h2>Current Tasks</h2>
+        {/* map sull' array di task da completare */}
+        {incompleteTasks.map((task, index) => {
+          return <li key={index}>{task.title}</li>;
+        })}
+      </ul>
+      {/* task completate */}
+      <ul>
+        <h2>Completed Tasks</h2>
+        {/*  */}
+        {completedTasks.map((task, index) => {
+          return <li key={index}>{task.title}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
-
 export default App;
