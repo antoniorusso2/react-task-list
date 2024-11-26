cbf47244a65f247717ff590410d3ef3de33f813e
antoniorusso2 <russoantonio303@outlook.it>
2024-11-26 18:24:16 +0100

    - updated css rules and classes | - added a counter for showing the number of tasks for each list | also added a banner with task progress info

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,51 @@ function App() {
   return (
     <div className="container">
       {/* task da completare */}
-      <ul className="task-list">
-        <h2>Current Tasks</h2>
+      <ul className="task-list todos">
+        <h2>
+          Current Tasks
+          <span className="counter"> ({incompleteTasks.length})</span>
+        </h2>
         {/* map sull' array di task da completare */}
         {incompleteTasks.map((task, index) => {
-          return (
+          return [
             <li key={index}>
-              <p className="task">{task.title}</p>
-              <p className="priority">Priority: {task.priority}</p>
-              <p className="estimated-time">
-                Estimated Time: {task.estimatedTime}
-              </p>
+              <div className="task-wrapper">
+                <h3 className="task">{task.title}</h3>
+                <p className="priority">Priority: {task.priority}</p>
+                <p className="estimated-time">
+                  Estimated Time: {task.estimatedTime}
+                </p>
+              </div>
+              <div className="banner" key={'banner'}>
+                {task.state}
+              </div>
             </li>
-          );
+          ];
         })}
       </ul>
       {/* task completate */}
-      <ul className="task-list">
-        <h2>Completed Tasks</h2>
+      <ul className="task-list done">
+        <h2>
+          Completed Tasks
+          <span className="counter"> ({completedTasks.length})</span>
+        </h2>
         {/*  */}
         {completedTasks.map((task, index) => {
-          return (
+          return [
             <li key={index}>
-              <p className="task">{task.title}</p>
-              <p className="priority">Priority: {task.priority}</p>
-              <p className="estimated-time">
-                Estimated Time: {task.estimatedTime}
-              </p>
+              <div className="task-wrapper">
+                <h3 className="task">{task.title}</h3>
+                <p className="priority">Priority: {task.priority}</p>
+                <p className="estimated-time">
+                  Estimated Time: {task.estimatedTime}
+                </p>
+              </div>
+              <div className="banner" key={'banner'}>
+                {task.state}
+              </div>
             </li>
-          );
+          ];
         })}
       </ul>
     </div>
