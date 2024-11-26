09a64b9bf3a9600b8bbea75b1856327c072806b9
antoniorusso2 <russoantonio303@outlook.it>
2024-11-26 17:25:32 +0100

    - css basic list style |

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import './App.css';
 import { tasks } from './data/tasks';
 
 const completedTasks = tasks.filter((task) => task.state === 'completed');
+// console.log(completedTasks);
 const incompleteTasks = tasks.filter((task) => task.state !== 'completed');
 
 function App() {
   return (
-    <>
+    <div className="container">
       {/* task da completare */}
       <ul className="task-list">
         <h2>Current Tasks</h2>
         {/* map sull' array di task da completare */}
         {incompleteTasks.map((task, index) => {
-          return <li key={index}>{task.title}</li>;
+          return [<li key={index}>{task.title}</li>];
         })}
       </ul>
       {/* task completate */}
@@ -23,7 +24,7 @@ function App() {
           return <li key={index}>{task.title}</li>;
         })}
       </ul>
-    </>
+    </div>
   );
 }
 export default App;
