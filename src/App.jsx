26242cc59890e4ff30558f750b868e2a224862fd
antoniorusso2 <russoantonio303@outlook.it>
2024-11-27 09:59:13 +0100

    - style for header

--- src/App.jsx
+++ src/App.jsx
@@ -8,71 +8,82 @@ function App() {
   const incompleteTasks = tasks.filter((task) => task.state !== 'completed');
 
   return (
-    <div className="container">
-      {/* task da completare */}
-      <ul className="task-list todos">
-        <h2>
-          Current Tasks
-          <span className="counter"> ({incompleteTasks.length})</span>
-        </h2>
-        {/* map sull' array di task da completare */}
-        {incompleteTasks.map((task, index) => {
-          return (
-            <li key={index}>
-              <div className="task-wrapper">
-                <h3 className="task">
-                  {task.title}
-                </h3>
+    <>
+      <header>
+        <div className="container">
+          <h1>Task Manager</h1>
+        </div>
+      </header>
 
-                <p className="priority">
-                  Priority: {task.priority}
-                </p>
+      <main>
+        <div className="container">
+          {/* task da completare */}
+          <ul className="task-list todos">
+            <h2>
+              Current Tasks
+              <span className="counter"> ({incompleteTasks.length})</span>
+            </h2>
+            {/* map sull' array di task da completare */}
+            {incompleteTasks.map((task, index) => {
+              return (
+                <li key={index}>
+                  <div className="task-wrapper">
+                    <h3 className="task">
+                      {task.title}
+                    </h3>
 
-                <p className="estimated-time">
-                  Estimated Time: {task.estimatedTime}
-                </p>
-              </div>
-              <div className="banner" key={'banner'}>
-                {task.state}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+                    <p className="priority">
+                      Priority: {task.priority}
+                    </p>
 
-      {/* task completate */}
-      <ul className="task-list done">
-        <h2>
-          Completed Tasks
-          <span className="counter"> ({completedTasks.length})</span>
-        </h2>
+                    <p className="estimated-time">
+                      Estimated Time: {task.estimatedTime}
+                    </p>
+                  </div>
+                  <div className="banner" key={'banner'}>
+                    {task.state}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
 
-        {/* 2nd elemento  */}
-        {completedTasks.map((task, index) => {
-          return (
-            <li key={index}>
-              <div className="task-wrapper">
-                <h3 className="task">
-                  {task.title}
-                </h3>
+          {/* task completate */}
+          <ul className="task-list done">
+            <h2>
+              Completed Tasks
+              <span className="counter"> ({completedTasks.length})</span>
+            </h2>
 
-                <p className="priority">
-                  Priority: {task.priority}
-                </p>
+            {/* 2nd elemento  */}
+            {completedTasks.map((task, index) => {
+              return (
+                <li key={index}>
+                  <div className="task-wrapper">
+                    <h3 className="task">
+                      {task.title}
+                    </h3>
 
-                <p className="estimated-time">
-                  Estimated Time: {task.estimatedTime}
-                </p>
-              </div>
+                    <p className="priority">
+                      Priority: {task.priority}
+                    </p>
 
-              <div className="banner" key={'banner'}>
-                {task.state}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                    <p className="estimated-time">
+                      Estimated Time: {task.estimatedTime}
+                    </p>
+                  </div>
+
+                  <div className="banner" key={'banner'}>
+                    {task.state}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+
+    </>
   );
 }
 export default App;
